cf45a17f59d12b87df403aff1ac848814070887e
Aleksander-Mass <aleksander.mass262@gmail.com>
2023-03-19 15:00:08 +0300

    First commit

---
+++ blog/blogapp/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Category, Post, Tag
+
+admin.site.register(Category)
+
+
+def clear_rating(modeladmin, request, queryset):
+    queryset.update(rating=1)
+
+
+clear_rating.short_description = "Выставить рейтинг = 1"
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['name', 'text', 'category', 'display_tags', 'has_image', 'rating']
+    actions = [clear_rating]
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag)
